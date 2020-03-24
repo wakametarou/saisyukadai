898753f5a0b7a08bcd3181643a1838979ccaa621
wakametarou <60685052+wakametarou@users.noreply.github.com>
2020-03-24 16:48:53 +0900

    6

--- db/migrate/20200317043604_change_data_codition_to_items.rb
+++ db/migrate/20200317043604_change_data_codition_to_items.rb
@@ -4,7 +4,7 @@ class ChangeDataCoditionToItems < ActiveRecord::Migration[5.2]
     change_column :items, :delivery_tax_payer, :string
     change_column :items, :delivery_from, :string
     change_column :items, :delivery_days, :string
-    change_column :items, :category, :string
+    change_column :items, :category_id, :string
     change_column :items, :brand, :string
   end
 end
