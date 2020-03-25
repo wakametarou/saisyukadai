f57ed30a6b1c2c3df8bfeaefc54f3be2ed396b63
SOKUTI <rsaangou.hirata@gmail.com>
2020-03-25 20:59:08 +0900

    Revert "Revert "fix""
    This reverts commit f09f0a30a8a1882f9ba318060c52b4981b174cd1.

--- db/schema.rb
+++ db/schema.rb
@@ -109,13 +109,13 @@ ActiveRecord::Schema.define(version: 2020_03_18_100204) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.date "birthday", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
