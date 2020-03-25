e66426cdc65838f620846a518ee3caa3090ab0f6
wakametarou <60685052+wakametarou@users.noreply.github.com>
2020-03-25 16:26:24 +0900

    1

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,21 @@
 ActiveRecord::Schema.define(version: 2020_03_18_100204) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "sendfirst_name", null: false
+    t.string "sendlast_name", null: false
+    t.string "sendfirst_name_kana", null: false
+    t.string "sendlast_name_kana", null: false
     t.integer "postal_code", null: false
-    t.integer "prefecture", null: false
+    t.string "prefecture", null: false
     t.string "city", null: false
     t.string "address_number", null: false
     t.string "building"
-    t.integer "user_id", null: false
+    t.integer "tell"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["tell"], name: "index_addresses_on_tell", unique: true
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -47,17 +48,13 @@ ActiveRecord::Schema.define(version: 2020_03_18_100204) do
     t.index ["user_id"], name: "index_dealings_on_user_id"
   end
 
-  create_table "item_brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "item_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.bigint "item_id"
     t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_item_categories_on_item_id"
     t.index ["name"], name: "index_item_categories_on_name"
   end
 
@@ -87,13 +84,16 @@ ActiveRecord::Schema.define(version: 2020_03_18_100204) do
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "tell"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
     t.text "introduce"
     t.string "avatar"
+    t.date "birthday", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.index ["tell"], name: "index_profiles_on_tell", unique: true
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -104,19 +104,21 @@ ActiveRecord::Schema.define(version: 2020_03_18_100204) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.date "birthday", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "dealings", "items"
   add_foreign_key "dealings", "users"
+  add_foreign_key "item_categories", "items"
   add_foreign_key "item_images", "items"
   add_foreign_key "items", "users"
   add_foreign_key "profiles", "users"
